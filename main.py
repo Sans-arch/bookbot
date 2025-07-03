e53451154ed05c07f8e6e627bb249d63c43c7f48
Sans-arch <sansfleitas@gmail.com>
2025-07-02 23:56:58 -0300

    create report structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_list_of_dictionaries
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -8,9 +8,17 @@ def get_book_text(file_path):
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     word_count = count_words(book_text)
-    print(f"{word_count} words found in the document")
-    
     character_count = count_characters(book_text)
-    print(character_count)
+    sorted_list = sort_list_of_dictionaries(character_count)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,6 @@ def count_words(text):
 
 def count_characters(text):
     result_dict = {}
-    
     for char in text:
         char = char.lower()
         if char in result_dict:
@@ -12,3 +11,17 @@ def count_characters(text):
             result_dict[char] = 1
         
     return result_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_list_of_dictionaries(chars_dict):
+    sorted_list = []
+    for char in chars_dict:
+        char_dict = {
+            "char": char,
+            "num": chars_dict[char]
+        }
+        sorted_list.append(char_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
